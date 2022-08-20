3a23aa83e7b79a64d08ed34a20abb0df895b0bba
conjurerofcode <miromoore@protonmail.com>
2022-08-19 19:55:25 -0700

    simplified design, adding loop feature

--- simple-ambience/App.tsx
+++ simple-ambience/App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SceneButton } from "./components/SceneButton";
 import { Scape, scapesList } from "./constants/Scape";
 import { Audio } from "expo-av";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import Animated, {
+  interpolateColor,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
+  withTiming,
 } from "react-native-reanimated";
 
-const colorz = ["#F44336", "#3F51B5", "#989998", "#4CAF50", "#80CBC4"];
+type LoopFunction = "LOOP" | "CYCLE" | "OFF";
+
+const colorz = [
+  "rgb(54, 56, 52)",
+  "rgb(80, 83, 166)",
+  "rgb(43, 17, 44)",
+  "rgb(97, 101, 36)",
+  "rgb(62, 176, 183)",
+];
 export default function App() {
   const [audio, setAudio] = useState<Audio.Sound>(new Audio.Sound());
   const [isPlaying, setIsPlaying] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
+  const [loopOrCycle, setLoopOrCycle] = useState<LoopFunction>("OFF");
+
+  const currIndex = useSharedValue(0);
+  const progress = useSharedValue(0);
+
+  const [bgColor, setBgColor] = useState(colorz[currIndex.value]);
+
+  // const animatedColor = useAnimatedStyle(() => {
+  //   const backgroundColor = interpolateColor(
+  //     progress.value,
+  //     [0, 1],
+  //     ["white", "black"]
+  //   );
+  //   return { backgroundColor };
+  // });
 
   let ran = false;
   const init = async () => {
@@ -41,6 +68,14 @@ export default function App() {
     setIsPlaying((prev) => !prev);
   };
 
+  const bgTransition = async (prevIndex: number, nextIndex: number) => {
+    // Animated.timing(progress, {
+    //   toValue: 1,
+    //   duration: 500
+    // }).start();
+    console.log(`change colors from index ${prevIndex} to ${nextIndex}`);
+  };
+
   const transition = async (newIndex: number) => {
     await audio.pauseAsync();
     await audio.unloadAsync();
@@ -50,23 +85,85 @@ export default function App() {
     setIsPlaying(true);
   };
 
-  const changeScene = async (idx: number) => {
-    if (idx == activeIndex) pausePlay();
+  const changeScene = async (newIdx: number) => {
+    console.log(progress.value);
+    const previousIndex = currIndex.value;
+    await bgTransition(previousIndex, newIdx);
+    if (newIdx == activeIndex) pausePlay();
     else {
-      console.log(`Activate: ${idx}`);
-      await setActiveIndex(idx);
-      transition(idx);
+      await setActiveIndex(newIdx);
+      currIndex.value = newIdx;
+      transition(newIdx);
+    }
+    console.log(`${scapesList[currIndex.value].bgColor}`);
+  };
+
+  function throttle(cb: Function, delay = 1000) {
+    let shouldWait = false;
+
+    return () => {
+      if (shouldWait) return;
+
+      cb();
+      shouldWait = true;
+
+      setTimeout(() => {
+        shouldWait = false;
+      }, delay);
+    };
+  }
+  const loopCycleHandler = () => {
+    switch (loopOrCycle) {
+      case "CYCLE":
+        setLoopOrCycle((prev) => "LOOP");
+        break;
+      case "LOOP":
+        setLoopOrCycle((prev) => "OFF");
+        break;
+      case "OFF":
+        setLoopOrCycle((prev) => "CYCLE");
+        break;
+      default:
+        break;
     }
   };
 
   const rColor = useAnimatedStyle(() => {
-    return { backgroundColor: colorz[activeIndex] };
+    // const backgroundColor = interpolateColor(
+    //   progress.value,
+    //   [0, 1],
+    //   [bgColor, colorz[activeIndex]]
+    // );
+
+    return { backgroundColor: colorz[currIndex.value] };
   });
+  // const rColor = useAnimatedStyle(() => {
+  //   return { backgroundColor: colorz[activeIndex] };
+  // });
 
   return (
-    <View style={styles.main}>
-      <Animated.View style={[styles.top, rColor]}></Animated.View>
-      <View style={styles.container}>
+    <Animated.View style={[styles.main, rColor]}>
+      <Animated.View style={[styles.top]}>
+        <Text style={[styles.title]}>Simple Ambience</Text>
+        <View style={styles.features}>
+          <TouchableOpacity
+            style={styles.loopButton}
+            onPress={loopCycleHandler}
+          >
+            {loopOrCycle === "OFF" && (
+              <Text style={{ fontSize: 20 }}>Loop / Cycle</Text>
+            )}
+            {loopOrCycle === "LOOP" && (
+              <MaterialIcons name="repeat" size={50} color="black" />
+            )}
+            {loopOrCycle === "CYCLE" && (
+              <MaterialIcons name="queue-play-next" size={50} color="black" />
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity></TouchableOpacity>
+        </View>
+      </Animated.View>
+      <View style={styles.sceneSelection}>
         <SceneButton index={0} scape={scapesList[0]} onPress={changeScene} />
         <SceneButton index={1} scape={scapesList[1]} onPress={changeScene} />
         <SceneButton index={2} scape={scapesList[2]} onPress={changeScene} />
@@ -74,28 +171,54 @@ export default function App() {
         <SceneButton index={4} scape={scapesList[4]} onPress={changeScene} />
       </View>
       <StatusBar style="auto" />
-    </View>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   main: {
     flex: 1,
-    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "space-evenly",
   },
-  container: {
+  top: {
+    height: "80%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    height: "100%",
+    width: "75%",
+    fontSize: 50,
+    color: "white",
+    textAlign: "center",
+    top: 100,
+  },
+  features: {
+    width: "100%",
+    height: "15%",
+    position: "absolute",
+    bottom: 0,
+    borderColor: "black",
+    borderWidth: 1,
+    justifyContent: "center",
+    paddingLeft: 10,
+  },
+  sceneSelection: {
     flex: 1,
-    backgroundColor: "grey",
     width: "100%",
     alignItems: "flex-start",
     justifyContent: "space-evenly",
     flexDirection: "row",
+    backgroundColor: "rgba(0, 0, 0, 0.25)",
     paddingTop: 20,
   },
-  top: {
-    height: "30%",
-    width: "100%",
+  loopButton: {
+    borderColor: "black",
+    borderWidth: 1,
+    width: "20%",
+    height: "80%",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- simple-ambience/components/SceneButton.tsx
+++ simple-ambience/components/SceneButton.tsx
@@ -7,7 +7,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { Scape } from "../constants/Scape";
+import { Scape, scapesList } from "../constants/Scape";
 
 interface ButtonProps {
   index: number;
@@ -27,12 +27,16 @@ export const SceneButton: React.FC<ButtonProps> = ({
   };
 
   return (
-    <Animated.View style={[styles.icon]}>
-      <TouchableOpacity onPress={pressHandler} style={styles.btn}>
-        {/* <ImageBackground
-              style={styles.img}
-              source={scape.icon}
-            ></ImageBackground> */}
+    <Animated.View style={[styles.icon, { backgroundColor: scape.bgColor }]}>
+      <TouchableOpacity
+        onPress={pressHandler}
+        style={[styles.btn]}
+        activeOpacity={1}
+      >
+        <ImageBackground
+          style={styles.img}
+          source={scape.icon}
+        ></ImageBackground>
       </TouchableOpacity>
       {/* <Animated.View style={[styles.shadow]}></Animated.View> */}
     </Animated.View>
@@ -41,29 +45,36 @@ export const SceneButton: React.FC<ButtonProps> = ({
 
 const styles = StyleSheet.create({
   icon: {
-    width: 50,
-    height: 50,
+    width: 40,
+    height: 40,
     borderRadius: 100,
     alignItems: "center",
-    backgroundColor: "blue",
+    justifyContent: "center",
     margin: 5,
   },
   btn: {
     height: "100%",
     width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
   },
   img: {
-    height: "100%",
-    width: "100%",
+    height: "110%",
+    width: "110%",
+    alignItems: "center",
+    justifyContent: "center",
+    bottom: 8,
+    right: 2,
   },
   shadow: {
     height: "25%",
     width: "25%",
     borderRadius: 5,
     position: "absolute",
-    bottom: -8,
+    bottom: -5,
     zIndex: -1,
     transform: [{ scaleX: 3 }],
     backgroundColor: "black",
+    opacity: 0.75,
   },
 });
